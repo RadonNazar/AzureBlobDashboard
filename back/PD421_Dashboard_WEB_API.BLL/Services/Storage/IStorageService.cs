c2052e1abe6bbed55b896191141fe3f709e3d2cd
RadonNazar <radon.bogdan09@gmail.com>
2025-11-08 11:55:23 +0200

    Add DeleteImageAsync method to IStorageService

--- back/PD421_Dashboard_WEB_API.BLL/Services/Storage/IStorageService.cs
+++ back/PD421_Dashboard_WEB_API.BLL/Services/Storage/IStorageService.cs
@@ -6,5 +6,7 @@ namespace PD421_Dashboard_WEB_API.BLL.Services.Storage
     {
         Task<string?> SaveImageAsync(IFormFile file, string folderPath);
         Task<IEnumerable<string>> SaveImagesAsync(IEnumerable<IFormFile> files, string folderPath);
+        
+        Task<bool> DeleteImageAsync(string blobPath); 
     }
 }
